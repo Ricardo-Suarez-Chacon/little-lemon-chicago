f283d1b12d54dc5b45b4157cdb387fadee63685b
Ricardo-Suarez-Chacon <Ricardosua@gmail.com>
2023-02-02 13:25:45 -0500

    form useState

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,14 +1,40 @@
+import { useState } from 'react';
 
 
 const BookingForm  = () => {
+    const [data, setData] = useState({})
 
+    const updateData = (e) =>{
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+        console.log(e.target.name,e.target.value)
+    }
+    const submit = e => {
+        e.preventDefault()
+        console.log(data)
+    }
     return(
         <>
-        <form className="Reservation-form">
-            <label for="res-date">Choose date</label>
-            <input type="date" id="res-date" />
-            <label for="res-time">Choose time</label>
-            <select id="res-time ">
+        <form 
+            className="Reservation-form"
+            onSubmit={submit}
+        >
+            <label htmlFor="res-date">Choose date</label>
+            <input
+                type="date"
+                id="res-date"
+                name="date"
+                onChange={updateData}
+            />
+            <label htmlFor="res-time">Choose time</label>
+            <select
+                id="res-time"
+                name="time"
+                onChange={updateData}
+            >
+                <option>Select Time</option>
                 <option>17:00</option>
                 <option>18:00</option>
                 <option>19:00</option>
@@ -16,12 +42,26 @@ const BookingForm  = () => {
                 <option>21:00</option>
                 <option>22:00</option>
             </select>
-            <label for="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" />
-            <label for="occasion">Occasion</label>
-            <select id="occasion">
+            <label htmlFor="guests">Number of guests</label>
+            <input
+                type="number"
+                placeholder="1"
+                min="1"
+                max="10"
+                id="guests"
+                name= "guests"
+                onChange={updateData}
+            />
+            <label htmlFor="occasion">Occasion</label>
+            <select 
+                id="occasion"
+                name="occasion"
+                onChange={updateData}
+                >
+                <option>Special Ocassion</option>
                 <option>Birthday</option>
                 <option>Anniversary</option>
+                <option>None</option>
             </select>
             <input type="submit" value="Make Your reservation" />
         </form>
@@ -29,4 +69,5 @@ const BookingForm  = () => {
 
     )
 }
+
 export default BookingForm

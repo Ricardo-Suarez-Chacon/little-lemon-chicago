3dd9630bf3f3025001d7e5585f09e92479d177d6
Ricardo-Suarez-Chacon <Ricardosua@gmail.com>
2023-02-03 18:14:37 -0500

    start unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('Renders the BookingForm heading', () => {
+  render(<BookingForm availableTimes={[]} dispatch={{}}/>);
+  const headingElement = screen.getByText("Choose date");
+  expect(headingElement).toBeInTheDocument();
 });
